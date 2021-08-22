8d149f4982855dcb9f41e059dd4292f7cf01011a
halima254 <halima.yahya@student.moringaschool.com>
2021-08-22 09:50:53 +0300

    add angular class quote

--- Quotes/src/app/app.component.ts
+++ Quotes/src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Quotes';
+  quotes: string[];
+  
+
+  constructor(){
+    this.quotes =['if it is meant to be','think out loud','promises']
+  }
 }
