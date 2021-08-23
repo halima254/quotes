f53c98d084bd7441fa641185ff7862f7e985b4fd
halima254 <halima.yahya@student.moringaschool.com>
2021-08-23 17:17:41 +0300

    display submission code

--- Quotes/src/app/quote/quote.component.ts
+++ Quotes/src/app/quote/quote.component.ts
@@ -14,10 +14,10 @@ export class QuoteComponent implements OnInit {
  }
 
   quotes: Quote[] = [
-    new Quote(1, 'Lisa', 'Be empty of worrying, think of who created thoughts', new Date(1994,3,5)),
-    new Quote(2, 'John', 'let yourself be silently drawn by the strange pill of what you really love. It will not lead you astray', new Date(2002,7,9)),
-    new Quote(3, 'Irene', 'Dance until you shatter yourself', new Date(2001, 2,1)),
-    new Quote(4, 'Phillip', 'This is a stable truth, whatever you love you are',new Date(1998,9,8)),
+    new Quote(1, 'Lisa', 'Be empty of worrying, think of who created thoughts', new Date(2012,3,5)),
+    new Quote(2, 'John', 'let yourself be silently drawn by the strange pill of what you really love. It will not lead you astray', new Date(2018,7,9)),
+    new Quote(3, 'Irene', 'Dance until you shatter yourself', new Date(2015, 2,1)),
+    new Quote(4, 'Phillip', 'This is a stable truth, whatever you love you are',new Date(2020,9,8)),
 
   ];
 
